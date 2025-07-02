25be8b0adcad7a5019effc8d6a7f35c53a3581ee
ranahusain <capthussain2point0@gmail.com>
2025-07-02 12:25:42 +0500

    signup remove from mainlayout

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import SignUp from "../pages/SignUp";
 
 const MainLayout = () => {
   return (
     <>
       <Outlet />
-      <SignUp />
       <ToastContainer theme="dark" />
     </>
   );
